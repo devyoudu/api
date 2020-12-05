05bf63d4f6e6c2131a8980f73ad7716671a2c854
Oscar <oscarfabiano@hotmail.com>
2020-12-05 16:01:39 -0500

    product images

--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -9,8 +9,7 @@ class ProductImageResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'image_url' => $this->image_url,
-            'thumb_image_url' => $this->thumb_image_url,
+            'image_name' => $this->image_url,
             'is_default' => $this->is_default,
         ];
     }
